eb30d0cc2085409522af32feaeb5f6bae77f7b77
HumzaKhatri <humza.khatri@astera.com>
2022-06-05 21:48:51 +0500

    Changed to a array and looping instead of hard coding

--- TaxSlab/ContentView.swift
+++ TaxSlab/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                 String(format: "%.0f", sliderValue)
             ).foregroundColor(color).padding()
             Slider(value: $sliderValue,
-                   in: 0...500000,
+                   in: 0...1000000,
                    step: 5000.0,
                    onEditingChanged: { _ in
                 taxValue = sliderChanged()/12
@@ -27,42 +27,35 @@ struct ContentView: View {
             Text(String(taxValue)).padding()
         }
     }
-    //tax slab for year 2021-2022
+    private struct TaxSlab{
+        let taxPercentage: Double
+        let yearlySalary: Double
+        let fixedTax: Double
+    }
+
+    //tax slabs for 2021-2022
+    private let TaxSlabs: [TaxSlab] = [
+        TaxSlab(taxPercentage: 35, yearlySalary: 75000000, fixedTax: 21420000),
+        TaxSlab(taxPercentage: 32.5, yearlySalary: 50000000, fixedTax: 13295000),
+        TaxSlab(taxPercentage: 30, yearlySalary: 30000000, fixedTax: 7295000),
+        TaxSlab(taxPercentage: 27.5, yearlySalary: 12000000, fixedTax: 2345000),
+        TaxSlab(taxPercentage: 25, yearlySalary: 8000000, fixedTax: 1345000),
+        TaxSlab(taxPercentage: 22.5, yearlySalary: 5000000, fixedTax: 670000),
+        TaxSlab(taxPercentage: 20, yearlySalary: 3500000, fixedTax: 370000),
+        TaxSlab(taxPercentage: 17.5, yearlySalary: 2500000, fixedTax: 195000),
+        TaxSlab(taxPercentage: 15, yearlySalary: 1800000, fixedTax: 90000),
+        TaxSlab(taxPercentage: 10, yearlySalary: 1200000, fixedTax: 30000),
+        TaxSlab(taxPercentage: 5, yearlySalary: 600000, fixedTax: 0)
+    ]
+    
     func sliderChanged() -> Double{
         let yearlySalary = sliderValue * 12
-        
-        if(yearlySalary > 75000000){
-            return calcTax(fixedAmmount: 21420000, difference: yearlySalary-75000000, perct: 35)
-        }
-        else if(yearlySalary > 50000000){
-            return calcTax(fixedAmmount: 13295000, difference: yearlySalary-50000000, perct: 32.5)
-        }
-        else if(yearlySalary > 30000000){
-            return calcTax(fixedAmmount: 7295000, difference: yearlySalary-30000000, perct: 30)
-        }
-        else if(yearlySalary > 12000000){
-            return calcTax(fixedAmmount: 2345000, difference: yearlySalary-12000000, perct: 27.5)
-        }
-        else if(yearlySalary > 8000000){
-            return calcTax(fixedAmmount: 1345000, difference: yearlySalary-8000000, perct: 25)
-        }
-        else if(yearlySalary > 5000000){
-            return calcTax(fixedAmmount: 670000, difference: yearlySalary-5000000, perct: 22.5)
-        }
-        else if(yearlySalary > 3500000){
-            return calcTax(fixedAmmount: 370000, difference: yearlySalary-3500000, perct: 20)
-        }
-        else if(yearlySalary > 2500000){
-            return calcTax(fixedAmmount: 195000, difference: yearlySalary-2500000, perct: 17.5)
-        }
-        else if(yearlySalary > 1800000){
-            return calcTax(fixedAmmount: 90000, difference: yearlySalary-1800000, perct: 15)
-        }
-        else if(yearlySalary > 1200000){
-            return calcTax(fixedAmmount: 30000, difference: yearlySalary-1200000, perct: 10)
-        }
-        else if(yearlySalary > 600000){
-            return calcTax(fixedAmmount: 0, difference: yearlySalary-600000, perct: 5)
+        var i: Int = 0
+        while(i < TaxSlabs.count){
+            if(yearlySalary > TaxSlabs[i].yearlySalary){
+                return calcTax(fixedAmmount: TaxSlabs[i].fixedTax, difference: yearlySalary-TaxSlabs[i].yearlySalary, perct: TaxSlabs[i].taxPercentage)
+            }
+            i = i+1
         }
         return 0
     }
