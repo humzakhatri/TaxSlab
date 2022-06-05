21e08211a705bb01302e0680e9319a4d72bc60a2
HumzaKhatri <humza.khatri@astera.com>
2022-06-05 16:37:42 +0500

    Initial Commit

---
+++ TaxSlab/TaxSlabApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxSlabApp.swift
+//  TaxSlab
+//
+//  Created by Humza on 05/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TaxSlabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
